9b12bb9f09e53018d8dffeff3d7c294b9de80e0c
Andrew C. Smith <andrewchristophersmith@gmail.com>
2018-03-30 20:36:49 -0700

    Remove my own directories from the build script

--- build.rs
+++ build.rs
@@ -5,15 +5,12 @@ use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rustc-link-lib=bela");
-    println!("cargo:rustc-link-lib=pthread_rt");
-    println!("cargo:rustc-link-lib=xenomai");
+    println!("cargo:rustc-link-lib=belaextra");
     println!("cargo:rustc-link-lib=native");
-    println!("cargo:rustc-link-lib=pthread");
+    println!("cargo:rustc-link-lib=xenomai");
     println!("cargo:rustc-link-lib=rt");
-    println!("cargo:rustc-link-lib=c");
     println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-search=all=C:/Users/andrewcsmith/workspaces/bin");
-    println!("cargo:rustc-link-search=all=C:/Users/andrewcsmith/workspaces/music/belars/lib");
+    println!("cargo:rustc-link-search=all=lib");
     let bindings = bindgen::Builder::default()
         .header("ext/Bela.h")
         .clang_arg("-Iinclude")
