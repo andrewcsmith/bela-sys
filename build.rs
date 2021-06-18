28135cefe6a6866c769f532dd03ea250319bc04a
l0calh05t <l0calh05t@gmx.net>
2021-06-18 23:16:12 +0200

    Use BELA_SYSROOT instead of ext/include/lib

--- build.rs
+++ build.rs
@@ -4,6 +4,16 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let bela_root = PathBuf::from(env::var("BELA_SYSROOT").unwrap_or_else(|_| "/".into()));
+    let bela_include = bela_root.join("root/Bela/include");
+    let bela_h = bela_include.join("Bela.h");
+    if !bela_h.exists() {
+        panic!("Set BELA_SYSROOT to the location of your extracted Bela image");
+    }
+    let bela_lib = bela_root.join("root/Bela/lib");
+    let xenomai_lib = bela_root.join("usr/xenomai/lib");
+    let local_lib = bela_root.join("usr/local/lib");
+
     #[cfg(feature = "static")]
     {
         println!("cargo:rustc-link-lib=static=bela");
@@ -15,15 +25,19 @@ fn main() {
     }
     println!("cargo:rustc-link-lib=cobalt");
     println!("cargo:rustc-link-lib=prussdrv");
+    println!("cargo:rustc-link-search=all={}", bela_lib.to_str().unwrap());
     println!(
-        "cargo:rustc-link-search=all={}/lib",
-        env::var("CARGO_MANIFEST_DIR").unwrap()
+        "cargo:rustc-link-search=all={}",
+        xenomai_lib.to_str().unwrap()
+    );
+    println!(
+        "cargo:rustc-link-search=all={}",
+        local_lib.to_str().unwrap()
     );
     let bindings = bindgen::Builder::default()
-        .header("ext/Bela.h")
-        .clang_arg("-Iinclude")
-        .clang_arg("-Iext")
-        .clang_arg("-fdeclspec")
+        .header(bela_h.to_str().unwrap())
+        .clang_arg(format!("--sysroot={}", bela_root.to_str().unwrap()))
+        .clang_arg(format!("-I{}", bela_include.to_str().unwrap()))
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
