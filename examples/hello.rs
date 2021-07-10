74ce5b8bc1812fbaf65fa8a8a71813333473e83f
l0calh05t <l0calh05t@gmx.net>
2021-07-10 16:54:10 +0200

    Finalize MIDI in examples/hello.rs

--- examples/hello.rs
+++ examples/hello.rs
@@ -9,6 +9,7 @@
 //! ssh root@bela.local "LD_LIBRARY_PATH=/root/Bela/lib /root/hello"
 //! ```
 //!
+
 extern crate bela_sys;
 extern crate libc;
 extern crate nix;
@@ -23,24 +24,55 @@ use bela_sys::midi::*;
 static mut FRAME_INDEX: usize = 0;
 
 #[cfg(feature = "midi")]
-static mut MIDI: *mut bela_sys::midi::Midi = std::ptr::null_mut();
+static mut MIDI: *mut bela_sys::midi::Midi = ptr::null_mut();
 
-#[no_mangle]
-pub unsafe extern "C" fn render(context: *mut BelaContext, _user_data: *mut std::os::raw::c_void) {
-    let n_frames = (*context).audioFrames;
-    let n_channels = (*context).audioOutChannels;
+extern "C" fn setup(_context: *mut BelaContext, _user_data: *mut std::os::raw::c_void) -> bool {
+    #[cfg(feature = "midi")]
+    unsafe {
+        assert!(MIDI.is_null());
+        MIDI = Midi_new(b"hw:0,0,0\0".as_ptr());
+    }
+    true
+}
 
-    let audio_out: &mut [f32] = slice::from_raw_parts_mut(
-        (*context).audioOut as *mut f32,
-        (n_frames * n_channels) as usize,
-    );
+extern "C" fn cleanup(_context: *mut BelaContext, _user_data: *mut std::os::raw::c_void) {
+    #[cfg(feature = "midi")]
+    unsafe {
+        Midi_delete(MIDI);
+        MIDI = std::ptr::null_mut();
+    }
+}
+
+extern "C" fn render(context: *mut BelaContext, _user_data: *mut std::os::raw::c_void) {
+    let context = unsafe { &mut *context };
+    let n_frames = context.audioFrames;
+    let n_channels = context.audioOutChannels;
+
+    #[cfg(feature = "midi")]
+    unsafe {
+        if !MIDI.is_null() {
+            while Midi_availableMessages(MIDI) > 0 {
+                let mut msg_buf = [0u8; 3];
+                let msg_len = Midi_getMessage(MIDI, msg_buf.as_mut_ptr()) as usize;
+                assert!(msg_len <= 3);
+                if msg_len != 0 && (msg_buf[0] & 0b1111_0000) == 0b1001_0000 {
+                    bela_sys::rt_printf(b"Note on\n\0".as_ptr());
+                }
+            }
+        }
+    }
+
+    let audio_out: &mut [f32] =
+        unsafe { slice::from_raw_parts_mut(context.audioOut, (n_frames * n_channels) as usize) };
+
+    let frame_index = unsafe { &mut FRAME_INDEX };
 
     let len = audio_out.len();
     for (idx, samp) in audio_out.iter_mut().enumerate() {
-        *samp = (idx as f32 / len as f32) * (FRAME_INDEX % 5) as f32;
+        *samp = (idx as f32 / len as f32) * (*frame_index % 5) as f32;
     }
 
-    FRAME_INDEX += 1;
+    *frame_index += 1;
 }
 
 extern "C" fn signal_handler(_signal: std::os::raw::c_int) {
@@ -57,16 +89,13 @@ fn main() {
             settings.assume_init()
         };
         bela_sys::Bela_setVerboseLevel(1);
+        settings.setup = Some(setup);
         settings.render = Some(render);
+        settings.cleanup = Some(cleanup);
         settings.verbose = 1;
         settings.highPerformanceMode = 1;
         settings.analogOutputsPersist = 0;
         settings.uniformSampleRate = 1;
-
-        #[cfg(feature = "midi")]
-        {
-            MIDI = Midi_new(b"hw:1,0,0\0".as_ptr());
-        }
 
         if bela_sys::Bela_initAudio(&mut settings, ptr::null_mut()) != 0 {
             println!("settings.render: {:?}", &settings.render);
@@ -94,11 +123,5 @@ fn main() {
 
         bela_sys::Bela_stopAudio();
         bela_sys::Bela_cleanupAudio();
-
-        #[cfg(feature = "midi")]
-        {
-            Midi_delete(MIDI);
-            MIDI = std::ptr::null_mut();
-        }
     }
 }
